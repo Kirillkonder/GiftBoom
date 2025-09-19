0510a77f6529bd290146c3c134ce46a6b1fe69b8
Kirillkonder <tehnoblogery754@gmail.com>
2025-09-19 21:50:18 +0300

    0.0.17

--- server.js
+++ server.js
@@ -826,7 +826,7 @@ function startRocketFlight() {
 
 
 
-unction processRocketGameEnd() {
+function processRocketGameEnd() {
     // Сохраняем игру в историю
     const gameRecord = rocketGames.insert({
         crashPoint: rocketGame.crashPoint,
